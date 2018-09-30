2f2b08b97d44a118ef436876a91a57839fb8a057
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-09-30 15:14:28 -0500

    Added digitalocean data

--- terrascript/digitalocean/d.py
+++ terrascript/digitalocean/d.py
@@ -1,4 +1,34 @@
 from terrascript import _data
-class digitalocean_image(_data): pass
-image = digitalocean_image
 
+class digitalocean_certificate(_data): pass
+data_certificate = digitalocean_certificate
+
+class digitalocean_domain(_data): pass
+data_domain = digitalocean_domain
+
+class digitalocean_droplet(_data): pass
+data_droplet = digitalocean_droplet
+
+class digitalocean_floating_ip(_data): pass
+data_floating_ip = digitalocean_floating_ip
+
+class digitalocean_image(_data): pass
+data_image = digitalocean_image
+
+class digitalocean_loadbalancer(_data): pass
+data_loadbalancer = digitalocean_loadbalancer
+
+class digitalocean_record(_data): pass
+data_record = digitalocean_record
+
+class digitalocean_ssh_key(_data): pass
+data_ssh_key = digitalocean_ssh_key
+
+class digitalocean_tag(_data): pass
+data_tag = digitalocean_tag
+
+class digitalocean_volume(_data): pass
+data_volume = digitalocean_volume
+
+class digitalocean_volume_snapshot(_data): pass
+data_volume_snapshot = digitalocean_volume_snapshot
